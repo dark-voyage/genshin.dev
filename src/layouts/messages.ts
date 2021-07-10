167723300ae1dedbc6e8f73110e91bcd4fdd4945
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-07-10 23:18:22 +0500

    updated start and message command messages

--- src/layouts/messages.ts
+++ src/layouts/messages.ts
@@ -3,6 +3,33 @@
  * @module layouts/messages
  */
 
-export const start = `Welcome to the Telegram bot of the Genshin Impact dear Traveler`
+export const start =
+  `<b>Welcome to Genshin Impact Telegram bot 😏!</b>` +
+  `\n` +
+  `\n` +
+  `Hi Sugar!) I'm a Genshin Impact assistant created by Genshindevs team that helps you to keep tracking information about Genshin Impact items, characters, guides ╰(*°▽°*)╯.` +
+  `\n` +
+  `\n` +
+  `With my help, you can do:` +
+  `\n` +
+  `\n` +
+  `<code>* Get information about items</code>` +
+  `\n` +
+  `<code>* Get informed about upcoming events</code>` +
+  `\n` +
+  `<code>* Check your item builds *coming soon*</code>` +
+  `\n` +
+  `<code>* Leave a feedback to admins</code>` +
+  `\n` +
+  `\n` +
+  `Hey, I hope we can get to know each other 😄` +
+  `\n` +
+  `\n` +
+  `<i>In order to see full detailed usage information of the bot, press the button below.</i>`
 
-export const help = `Some help message here`
+export const help =
+  `<b>List of available commands:</b>` +
+  `\n` +
+  `\n` +
+  `/help - <code>show this helper message</code>` +
+  `\n`
