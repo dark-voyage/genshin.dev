56307e179362e72c8902d1f3303835a15660af9f
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-07-12 20:43:15 +0500

    simple character system

--- src/actions/api/characters/action.ts
+++ src/actions/api/characters/action.ts
@@ -3,15 +3,24 @@ import * as consoles from '@layouts/consoles'
 import * as message from '@layouts/messages'
 import * as keyboard from '@layouts/keyboards'
 import { TelegrafContext } from '@type/telegraf'
-import * as cs from "@database/cs";
+import * as cs from '@database/cs'
+import ds from '@database/ds'
 
 composer.action(
     'characters',
     async (ctx: TelegrafContext): Promise<void> => {
-        await ctx.editMessageText(message.characters.middleware, {
-            parse_mode: 'HTML',
-            reply_markup: await keyboard.characters.middleware()
-        })
+        await ctx.editMessageMedia(
+            {
+                type: 'photo',
+                media:
+                    'https://raw.githubusercontent.com/genshindev/telegram-bot/master/assets/characters.jpg',
+                caption: message.characters.middleware
+            },
+            {
+                parse_mode: 'HTML',
+                reply_markup: await keyboard.characters.middleware()
+            }
+        )
     }
 )
 
@@ -19,10 +28,11 @@ composer.action(
     /character_(.*)/gi,
     async (ctx: TelegrafContext): Promise<void> => {
         const slug = ctx.match[1]
+        const image = cs.CHARACTER_URL + `/${slug}/portrait`
         await ctx.editMessageMedia(
             {
                 type: 'photo',
-                media: cs.CHARACTER_URL + `/${slug}`,
+                media: { url: image, filename: '' },
                 caption: await message.characters.action(slug)
             },
             {
--- src/actions/api/characters/middleware.ts
+++ src/actions/api/characters/middleware.ts
@@ -7,11 +7,14 @@ import { TelegrafContext } from '@type/telegraf'
 composer.command(
     'characters',
     async (ctx: TelegrafContext): Promise<void> => {
-        await ctx.replyWithPhoto(``, {
-          parse_mode: 'HTML',
-          caption: message.characters.middleware,
-          reply_markup: await keyboard.characters.middleware()
-        })
+        await ctx.replyWithPhoto(
+            `https://raw.githubusercontent.com/genshindev/telegram-bot/master/assets/characters.jpg`,
+            {
+                parse_mode: 'HTML',
+                caption: message.characters.middleware,
+                reply_markup: await keyboard.characters.middleware()
+            }
+        )
     }
 )
 
